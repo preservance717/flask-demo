313c50ff3a3acbb80890090c55705fd4441f7285
LG <GL717@outlook.com>
2017-10-19 16:10:10 +0800

    learn http method

---
+++ http-demo.py
@@ -0,0 +1,18 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+def do_the_login():
+    return 'do the login POST'
+
+def show_the_login_form():
+        return 'show the login form GET'
+
+if __name__ == '__main__':
+    app.run(debug=True)
